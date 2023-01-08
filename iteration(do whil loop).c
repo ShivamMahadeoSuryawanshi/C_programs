6354c800865c17095a997760c414af51efec1cfa
Shivam Mahadeo Suryawanshi <122144886+ShivamMahadeoSuryawanshi@users.noreply.github.com>
2023-01-08 20:47:29 +0530

    Add files via upload

---
+++ iteration(do whil loop).c
@@ -0,0 +1,18 @@
+// Print hii by using iteration (by  do while loop) 
+
+#include<stdio.h>
+
+int main()
+{
+  int iCnt=0;
+ 
+  iCnt=1;
+  do
+  {
+   printf("Hi...\n ");
+   iCnt++;
+   } while(iCnt<=10);
+  
+   
+   return 0;
+}
