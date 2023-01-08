2241e066f27bf8acf21ded03be0709058350150d
Shivam Mahadeo Suryawanshi <122144886+ShivamMahadeoSuryawanshi@users.noreply.github.com>
2023-01-08 20:07:24 +0530

    Add files via upload

---
+++ Iteration(for loop).c
@@ -0,0 +1,15 @@
+// Print hii 5 type using iteration (for loop).
+
+#include<stdio.h>
+
+int main()
+{
+  int iCnt=0;
+  
+  for (iCnt=1; iCnt<6; iCnt++) //type of iteration. its same in c,c++ as well as java.
+  {
+   printf("Hii... \n ");
+   }
+   
+   return 0;
+}
