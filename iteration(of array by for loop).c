0b1afd176612b3addd2f66210e0ed65bcbb7e0ab
Shivam Mahadeo Suryawanshi <122144886+ShivamMahadeoSuryawanshi@users.noreply.github.com>
2023-01-08 20:11:28 +0530

    Add files via upload

---
+++ iteration(of array by for loop).c
@@ -0,0 +1,20 @@
+// Print element of arry using iteration (for loop).
+
+#include<stdio.h>
+
+int main()
+{
+  int arr[4]={10,11,12,13};
+  int icnt = 0;
+	
+   printf("element of arry is \n");
+   
+   for(icnt=0; icnt<=3;icnt++)
+   
+   {
+     printf("%d\n",arr[icnt]);
+   }
+   
+  
+   return 0;
+}
