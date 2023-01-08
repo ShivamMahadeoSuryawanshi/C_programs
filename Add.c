aac33c7bfdee6fcb16fdf142d672ffc56ce59481
Shivam Mahadeo Suryawanshi <122144886+ShivamMahadeoSuryawanshi@users.noreply.github.com>
2023-01-08 12:28:17 +0530

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,17 @@
+// Addition of two int inside the main.
+#include<stdio.h>
+
+int main()
+{
+	int No1=10;
+	
+	int No2=11;
+	
+	int Ans=0;
+	
+	Ans=No1 + No2; // logic
+	
+	printf("%d\n",Ans); // print the final  value.
+	
+	return 0;
+}
